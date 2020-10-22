21287e8d86e2d61eb80af8db18e12087d374e660
ksnb <4644sheng@sina.com>
2020-10-22 14:12:48 +0800

    添加声明周期

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 import { createElement, Component, render } from  './toy-react';
 
-class MyComponent extends Component{
-    render() {
-        return  <div>
-            <h1>my component</h1>
-            { this.children }   
-        </div>
-    }
-  
-}
+// render方法，给component的实例root赋值 
+// class MyComponent extends Component{
+//     constructor() {
+//         super();  // 调用父类构造函数
+//         this.state = {
+//             a:1,
+//             b:2,
+//         }
+//     }
+//     render() {
+//         return  <div>
+//             <h1>my component</h1>
+//             <button onclick={() => { this.setState({a: this.state.a +1 }) }}>add</button>
+//             <span>{this.state.a.toString()}</span>
+//             <span>{this.state.b.toString()}</span>
+//         </div>
+//     }
+   
+// }
 /* var a = <div id="a" class="c">
     <div></div>
     <div></div>
@@ -34,10 +44,146 @@ class MyComponent extends Component{
 // 将a 挂载到 body上
 // document.body.appendChild(a)   // 当有文本内容的时候，需要判断
 
-render(<MyComponent>
+// render(<MyComponent>
+//     <div>abc</div>
+//     <div></div>
+//     <div></div>
+// </MyComponent>,document.body);
+
+// 根据toy-react传入render函数 相当于document.body.appendChild()
+/*****
+ * <MyComponent>
     <div>abc</div>
     <div></div>
-    <div></div>
-</MyComponent>,document.body)
+    <></>
+</MyComponent>
+ * 
+ * MyComponent.root 就是他的返回  <div>
+            <h1>my component</h1>
+            { this.children }   
+        </div>
 
+        div以及H1都会转化为 ElementWrapper 解析
+        my component  转化为 TextWrapper 解析
+        this.children 相当于占位符，会将<div>abc</div>
+    <div></div>
+    <div></div> 当做children push到component的child中，
+ */
+
+
+//  Tic Tac Toe
+class Square extends Component {
+  render () {
+    return   (
+  <button className="square" onClick={this.props.onClick}>
+    {this.props.value}
+  </button>
+);
+} 
+}
+
+class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  render() {
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+render(
+  <Game />,
+  document.getElementById('root')
+);
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 
--- toy-react.js
+++ toy-react.js
@@ -1,67 +1,120 @@
-class  ElementWrapper {
-    // 处理元素的情况
+//SUCCESS 总结 其实就是   createElement函数的应对
+
+const RENDER_TO_DOM = Symbol('render to dom');
+class ElementWrapper {
+    // 处理元素的情况 
     constructor(type) {
         this.root = document.createElement(type);
     }
-    setAttribute(name,value) {
-        this.root.setAttribute(name,value);
+    setAttribute(name, value) {
+        if (name.match(/^on([\s\S]+)$/)) { // 匹配型，RegExp.$1 就是匹配的值
+            this.root.addEventListener(RegExp.$1.replace(/^[\s\S]/, c => c.toLocaleLowerCase()), value, false)
+        } else {
+            // 处理className 
+            if (name === 'className') {
+                this.root.setAttribute('class', value);
+            } else {
+
+                this.root.setAttribute(name, value);
+            }
+        }
     }
-    appendChild (component) {  // 传入一个组件，加入组件的root
-        this.root.appendChild(component.root);
-    }
+    appendChild(component) { // 传入一个组件，加入组件的root
+            console.log(component, '-------component');
+            let range = document.createRange();
+            range.setStart(this.root, this.root.childNodes.length);
+            range.setEnd(this.root, this.root.childNodes.length);
+            component[RENDER_TO_DOM](range);
+        }
+        [RENDER_TO_DOM](range) { // dom关于位置的api ---range  不再单纯的使用appendChild，
+            range.deleteContents();
+            range.insertNode(this.root);
+        }
 }
 
 class TextWrapper {
-     //TODO  TextWrapper 目前还留有疑惑
-     constructor(content) {
-        this.root = document.createTextNode(content);
-    }
+    // 文本createElement 文本类型
+    constructor(content) {
+            this.root = document.createTextNode(content);
+        }
+        [RENDER_TO_DOM](range) { // dom关于位置的api ---range  不再单纯的使用appendChild，
+            range.deleteContents();
+            range.insertNode(this.root);
+        }
 }
 
 export class Component {
-    constructor( ) {
+    // 处理 createElement组件类型
+    constructor() {
         this.props = Object.create(null);
         this.children = [];
         this._root = null;
+        this._range = null;
     }
-    setAttribute(name,value) {
+    setAttribute(name, value) {
         this.props[name] = value;
     }
-    appendChild (component) {  // 传入一个组件，加入组件的root
-        this.children.push(component);
-    }
-    get root() {
-        if(!this._root) {
-            this._root = this.render().root;  // 如果返回的依然是component那么会继续递归取root
+    appendChild(component) { // 传入一个组件，加入组件的root
+            this.children.push(component);
         }
-        return this._root;
+        [RENDER_TO_DOM](range) { // dom关于位置的api ---range  不再单纯的使用appendChild，
+            this._range = range;
+            this.render()[RENDER_TO_DOM](range); // 如果还是component的子类还是能够递归
+        }
+    rerender() {
+        this._range.deleteContents();
+        this[RENDER_TO_DOM](this._range);
     }
+    setState(newState) {
+        console.log('1111', newState);
+        if (this.state === null && typeof this.state !== 'object') {
+            this.state = newState;
+            this.rerender();
+            return;
+        }
+        let merge = (oldState, newState) => {
+            for (let p in newState) {
+                if (oldState[p] === null || typeof oldState[p] !== 'object') {
+                    oldState[p] = newState[p];
+                } else {
+                    merge(oldState[p], newState[p])
+                }
+            }
+        }
+        merge(this.state, newState)
+        this.rerender();
+    }
+
 }
 
-export function  createElement(type,attributes,...children) {
+export function createElement(type, attributes, ...children) {
     let e;
-    if(typeof type === 'string') {
+    if (typeof type === 'string') {
 
         e = new ElementWrapper(type);
     } else {
-        e = new type;  // 实际上，置于对原生dom的api进行封装才能真正的跑通，否则自己是没办法讲一个类，
+        e = new type; // 实际上，置于对原生dom的api进行封装才能真正的跑通，否则自己是没办法讲一个类，
         // 包装成一个原生的dom元素扥                  
     }
     // attributes 是对象
     for (let p in attributes) {
-        e.setAttribute(p,attributes[p]);  // 通过15行 20行  27 可以分析得到，我们创建的实例需要包含setAttribute 和 appendChild  两个方法
+        e.setAttribute(p, attributes[p]); // 通过15行 20行  27 可以分析得到，我们创建的实例需要包含setAttribute 和 appendChild  两个方法
     }
     // children 是子元素数组集合
     let insertChildren = (children) => {
 
-        for(let child of children) {
-            if(typeof child === 'string') {
-                child = new TextWrapper(child);
+        for (let child of children) {
+            if (typeof child === 'string') {
+                child = new TextWrapper(child); // 文本节点
             }
-            if(typeof child === 'object' && (child instanceof Array )) {
-                insertChildren(child);
-            } else {
+            if (child === null) {
 
+                continue;
+            }
+            if (typeof child === 'object' && (child instanceof Array)) {
+                insertChildren(child); // 子元素时数组，继续递归
+            } else {
+                // 如果递归完毕
                 e.appendChild(child);
             }
         }
@@ -71,6 +124,10 @@ export function  createElement(type,attributes,...children) {
 }
 
 
-export  function render(component,parentNode) {
-    parentNode.appendChild(component.root)
+export function render(component, parentNode) {
+    let range = document.createRange();
+    range.setStart(parentNode, 0);
+    range.setEnd(parentNode, parentNode.childNodes.length);
+    range.deleteContents();
+    component[RENDER_TO_DOM](range);
 }
